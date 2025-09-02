18f172e4cb441d7bd87a53624849150d8c4bd675
Shneor <mm4471661@gmail.com>
2025-09-02 17:40:48 +0300

    commit 1

---
+++ src/comp/Home.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <>
+    <h1>Riddle Game</h1>
+
+    <div className="main-menu">
+        <div>Play Game</div>
+        <div>Login</div>
+        <div>Leaderboard</div>
+    </div>
+
+    <footer>Riddle Game v1.0</footer>
+    </>
+  )
+}
