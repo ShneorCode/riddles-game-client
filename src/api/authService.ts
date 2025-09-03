e1d9245ef577a2e5027976fe0a562ed04a7b67bf
Shneor <mm4471661@gmail.com>
2025-09-03 11:08:15 +0300

    commit-2

--- src/api/authService.ts
+++ src/api/authService.ts
@@ -1,4 +1,4 @@
-import { User } from '../types';
+import type { User } from '../types';
 
 const AUTH_URL = 'http://localhost:3007/api/auth/login';
 const REGISTER_URL = 'http://localhost:3007/api/auth/register';
